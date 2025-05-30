1b29903b44bb620fee177b219bc1cd89f0e9b483
vsm <vsmoura2703@gmail.com>
2025-05-30 00:25:51 -0300

    opçao d colocar imagens no guestbook + algumas alteraçoes no server

--- server.js
+++ server.js
@@ -49,8 +49,7 @@ app.get("/", (req, res) => {
 app.post("/messages", upload.single("image"), async (req, res) => {
   try {
     const { name, message, textColor, bgColor } = req.body;
-    const imageUrl = req.file ? `${req.protocol}://${req.get('host')}/uploads/${req.file.filename}` : null;
-
+    const imageUrl = req.file ? `https://vm-webcorner.onrender.com/uploads/${req.file.filename}` : null;
 
     if (!name || (!message && !req.file)) {
       return res.status(400).json({ error: "mensagem ou imagem são obrigatórios." });
